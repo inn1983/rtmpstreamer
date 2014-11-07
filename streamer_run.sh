2036cbe74a623411ffaf33859dcb929fb3ec3c87
inn1983 <inngi2011@gmail.com>
2014-11-08 08:08:07 +0900

    enable alsa capture.

--- streamer_run.sh
+++ streamer_run.sh
@@ -8,6 +8,9 @@ cd /home/cubie/wkdir/rtmptest/crtmp/builders/cmake
 su -s /bin/bash -c "crtmpserver/crtmpserver crtmpserver/crtmpserver.lua" cubie
 sleep 5
 
+#enable alsa capture.
+amixer -c 0 cset numid=25 0
+
 cd /home/cubie/wkdir/rtmpstreamer
 
 chmod 777 /dev/sunxi_mem
@@ -22,7 +25,7 @@ chown cubie:cubie /dev/sunxi_mem
 
 #su -s /bin/bash -c "arecord -Dplughw:0,0 -fS16_LE -r44100 -c1 > WavInFifo.wav 2>arec.txt &" cubie
 #sleep 2
-su -s /bin/bash -c "./rtmp_streamer rtmp://192.168.2.107/flvplayback/streamer>/dev/null 2>./error.log&" cubie
+su -s /bin/bash -c "./rtmp_streamer rtmp://localhost/flvplayback/streamer>/dev/null 2>./error.log&" cubie
 #su -s /bin/bash -c "./rtmp_streamer rtmp://192.168.2.107/flvplayback/streamer > /dev/null 2>&1 &" cubie
 #su -s /bin/bash -c "nohup ./rtmp_streamer rtmp://172.16.200.1/flvplayback/streamer &" cubie
 #su -s /bin/bash -c "./rtmp_streamer rtmp://192.168.2.107/flvplayback/streamer > ./log.txt 2>./log.txt &" cubie
