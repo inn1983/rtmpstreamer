89998e6943057f816738713ff62799d47d8c5b80
inn1983 <inngi2011@gmail.com>
2014-11-10 17:36:02 +0900

    When send error print out the timestamp.

--- RTMPStream.cpp
+++ RTMPStream.cpp
@@ -1154,6 +1154,7 @@ bool CRTMPStream::SendCapEncode(void)
 			char tmp[64];
 			strftime( tmp, sizeof(tmp), "%Y%m%d%H%M%S_log.txt", localtime(&t) ); //格式化输出.
 			fprintf(stderr, "time:%s, line:%d ::Send error!!\n", tmp, __LINE__);
+			fprintf(stderr, "naluUnit.size:%d, naluUnit.pts:%lld.", naluUnit.size, naluUnit.pts);
 			
 			while(1){
 				LOGD(g_debuglog, "Rtmp restart !!");
